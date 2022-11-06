73fd618fdda49be1632dc1cde5acc5580a891887
manuel paesano <mepaesano24@ucema.edu.ar>
2022-11-06 19:08:49 -0300

    update noticias

--- src/components/pages/Noticias.js
+++ src/components/pages/Noticias.js
@@ -2,7 +2,77 @@ import React from 'react'
 
 const Noticias = () => {
   return (
-    <h1>Noticias</h1>
+    <div>
+    <div className='row row-cols-1 row-cols-md-3 g-4'>
+      <div className='col'>
+        <div className='card'>
+        <a href="https://cripto247.com/comunidad-cripto/europa-prohibe-las-transferencias-de-cualquier-criptomoneda-a-rusia-224055/"> 
+          <img src='https://www.criptonoticias.com/wp-content/uploads/2022/01/rusia-busca-prohibir-compra-venta-mineria-bitcoin-1140x570.jpg' className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>Card title</h5>
+            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          </a>
+        </div>
+      </div>
+      <div className='col'>
+        <div className='card'>
+        <a href="https://cripto247.com/comunidad-cripto/el-futbol-y-las-criptomonedas-una-relacion-que-no-para-de-crecer-224956/"> 
+          <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/11/pelota-oficial-copa-mundial-de-la-fifa-qatar-2022_862x485-728x410.webp' className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>Card title</h5>
+            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          </a>
+        </div>
+      </div>
+      <div className='col'>
+        <div className='card'>
+        <a href="https://www.cripto247.com/comunidad-cripto/mercado-libre-lanzo-el-servicio-de-compraventa-de-criptomonedas-en-mexico-224769/"> 
+          <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/10/mexico-3949332_12801111-728x410.webp' className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>Card title</h5>
+            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          </a>
+        </div>
+      </div>
+      <div className='col'>
+        <div className='card'>
+        <a href="https://www.laprensagrafica.com/economia/Restringen-transacciones-en-criptomonedas-desde-El-Salvador-20221104-0087.html"> 
+          <img src='https://elceo.com/wp-content/uploads/2021/06/bitcoinsal.jpg' className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>Card title</h5>
+            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          </a>
+        </div>
+      </div>
+      <div className='col'>
+        <div className='card'>
+        <a href="https://cripto247.com/comunidad-cripto/la-cvm-de-brasil-reconoce-que-hay-un-vacio-en-la-regulacion-y-clasifica-las-criptomonedas-en-tres-clases-de-activos-224234/"> 
+          <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/10/411-728x410.webp' className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>Card title</h5>
+            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          </a>
+        </div>
+      </div>
+      <div className='col'>
+        <div className='card'>
+        <a href="https://cripto247.com/comunidad-cripto/argentina-aduana-incauta-342-equipos-de-mineria-de-criptomonedas-224593/"> 
+          <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/10/granja-ilegal1-728x410.webp' className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>Card title</h5>
+            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div> 
+
   )
 }
 
