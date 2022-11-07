6872ec65b1574a5a69004fdb251ffa04272f68aa
Juanpaesano <juanchopaesano@gmail.com>
2022-11-07 10:25:00 -0300

    mas noticias, falta css

--- src/components/pages/Noticias.js
+++ src/components/pages/Noticias.js
@@ -9,8 +9,8 @@ const Noticias = () => {
         <a href="https://cripto247.com/comunidad-cripto/europa-prohibe-las-transferencias-de-cualquier-criptomoneda-a-rusia-224055/"> 
           <img src='https://www.criptonoticias.com/wp-content/uploads/2022/01/rusia-busca-prohibir-compra-venta-mineria-bitcoin-1140x570.jpg' className='card-img-top' alt='...' />
           <div className='card-body'>
-            <h5 className='card-title'>Card title</h5>
-            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 className='card-title'>Rusia</h5>
+            <p className='card-text'>Europa prohíbe las transferencias de cualquier criptomoneda a Rusia</p>
           </div>
           </a>
         </div>
@@ -20,8 +20,8 @@ const Noticias = () => {
         <a href="https://cripto247.com/comunidad-cripto/el-futbol-y-las-criptomonedas-una-relacion-que-no-para-de-crecer-224956/"> 
           <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/11/pelota-oficial-copa-mundial-de-la-fifa-qatar-2022_862x485-728x410.webp' className='card-img-top' alt='...' />
           <div className='card-body'>
-            <h5 className='card-title'>Card title</h5>
-            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 className='card-title'>Futbol</h5>
+            <p className='card-text'>El fútbol y las criptomonedas: una relación que no para de crecer</p>
           </div>
           </a>
         </div>
@@ -31,8 +31,8 @@ const Noticias = () => {
         <a href="https://www.cripto247.com/comunidad-cripto/mercado-libre-lanzo-el-servicio-de-compraventa-de-criptomonedas-en-mexico-224769/"> 
           <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/10/mexico-3949332_12801111-728x410.webp' className='card-img-top' alt='...' />
           <div className='card-body'>
-            <h5 className='card-title'>Card title</h5>
-            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 className='card-title'>Mercado Libre</h5>
+            <p className='card-text'>Mercado Libre lanzó el servicio de compraventa de criptomonedas en México</p>
           </div>
           </a>
         </div>
@@ -42,8 +42,8 @@ const Noticias = () => {
         <a href="https://www.laprensagrafica.com/economia/Restringen-transacciones-en-criptomonedas-desde-El-Salvador-20221104-0087.html"> 
           <img src='https://elceo.com/wp-content/uploads/2021/06/bitcoinsal.jpg' className='card-img-top' alt='...' />
           <div className='card-body'>
-            <h5 className='card-title'>Card title</h5>
-            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 className='card-title'>Restricción El Salvador</h5>
+            <p className='card-text'>Restringen transacciones en criptomonedas desde El Salvador</p>
           </div>
           </a>
         </div>
@@ -53,8 +53,8 @@ const Noticias = () => {
         <a href="https://cripto247.com/comunidad-cripto/la-cvm-de-brasil-reconoce-que-hay-un-vacio-en-la-regulacion-y-clasifica-las-criptomonedas-en-tres-clases-de-activos-224234/"> 
           <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/10/411-728x410.webp' className='card-img-top' alt='...' />
           <div className='card-body'>
-            <h5 className='card-title'>Card title</h5>
-            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 className='card-title'>Comisión de Valores Brasil</h5>
+            <p className='card-text'>La CVM de Brasil reconoce que hay un vacío en la regulación y clasifica las criptomonedas en tres clases de activos</p>
           </div>
           </a>
         </div>
@@ -64,14 +64,47 @@ const Noticias = () => {
         <a href="https://cripto247.com/comunidad-cripto/argentina-aduana-incauta-342-equipos-de-mineria-de-criptomonedas-224593/"> 
           <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/10/granja-ilegal1-728x410.webp' className='card-img-top' alt='...' />
           <div className='card-body'>
-            <h5 className='card-title'>Card title</h5>
-            <p className='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 className='card-title'>Minería</h5>
+            <p className='card-text'>Argentina: Aduana incauta 342 equipos de minería de criptomonedas</p>
           </div>
           </a>
         </div>
       </div>
     </div>
-  </div> 
+    <div className='col'>
+        <div className='card'>
+        <a href="https://cripto247.com/comunidad-cripto/el-gobierno-de-china-tiene-mas-bitcoins-que-cualquier-empresa-privada-224925/"> 
+          <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/11/wood-door-1711004_1920-728x410.webp' className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>China acumula btc</h5>
+            <p className='card-text'>El Gobierno de China tiene más bitcoins que cualquier empresa privada</p>
+          </div>
+          </a>
+        </div>
+      </div>
+      <div className='col'>
+        <div className='card'>
+        <a href="https://cripto247.com/comunidad-cripto/importante-banco-restringe-las-transferencia-cripto-a-exchanges-224970/"> 
+          <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/11/transferenciabtc-728x410.webp' className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>Santander Reino Unido</h5>
+            <p className='card-text'>Importante banco restringe las transferencia cripto a exchanges</p>
+          </div>
+          </a>
+        </div>
+      </div>
+      <div className='col'>
+        <div className='card'>
+        <a href="https://cripto247.com/comunidad-cripto/los-estrategas-de-jp-morgan-creen-que-el-ciclo-bajista-aun-esta-lejos-de-su-fin-224947/"> 
+          <img src='https://cripto24.s3.amazonaws.com/media/uploads/2022/11/clima-tiempo-pronostico-frio-lluvias-invierno-2021-argentina-339701-1_1280-728x410.webp' className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>Tendencia de mercado</h5>
+            <p className='card-text'>Los estrategas de JP Morgan creen que el ciclo bajista aún está lejos de su fin</p>
+          </div>
+          </a>
+        </div>
+      </div>
+    </div>
 
   )
 }
